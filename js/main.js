da2fa3c31934e7d40190eb4dc90e56b1e8cd4b4f
rastereo <kaptur-rostislav@mail.ru>
2021-10-16 13:27:22 +0300

    добавил switch-active-form.js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@ import './data.js';
 import './random-generator.js';
 import { createProperty } from './create-property.js';
 import { createPopup } from './popup.js';
+import { switchActiveForm } from './switch-active-form.js';
 
 createPopup(createProperty[0]);
+switchActiveForm();
